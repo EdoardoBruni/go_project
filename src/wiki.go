d12cec1d3746dc958e0d4f7828dff7811a5be9b7
edoardo.bruni <edoardo.bruni91@gmail.com>
2018-06-21 09:30:47 +0200

    sesto commit

--- src/wiki.go
+++ src/wiki.go
@@ -28,7 +28,11 @@ func loadPage (title string) (*Page,error) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
 	renderTemplate(w,"view",p)
 }
 
@@ -39,6 +43,14 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 		p=&Page{Title: title}
 	}
 	renderTemplate(w,"edit",p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+ title, http.StatusFound) 
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
