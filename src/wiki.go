80f909826892769d54cd31dd1817061c771d4629
edoardo.bruni <edoardo.bruni91@gmail.com>
2018-06-21 11:53:06 +0200

    ottavo commit

--- src/wiki.go
+++ src/wiki.go
@@ -17,7 +17,7 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename,p.Body,0600)
 }
 
-func loadPage (title string) (*Page,error) {
+func loadPage(title string) (*Page,error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -49,19 +49,27 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	p.save()
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	http.Redirect(w, r, "/view/"+ title, http.StatusFound) 
 }
 
+var templates = template.Must(template.ParseFiles("edit.html","view.html"))
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w,p)
+	err := templates.ExecuteTemplate(w, tmpl+".html",p)
+	if err != nil {
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+	}
 }
 
 func main() {
 	http.HandleFunc("/view/",viewHandler)
 	http.HandleFunc("/edit/",editHandler)
-	//http.HandleFunc("/save/",saveHandler)
+	http.HandleFunc("/save/",saveHandler)
         log.Fatal(http.ListenAndServe(":8080",nil))
 }
 
